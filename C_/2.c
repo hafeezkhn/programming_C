cbff915c1c3f5eddce58e05e7a4ee71170c15be7
hafeezkhn <hafeez017@outlook.com>
2021-07-05 00:01:43 +0530

    initial commit

---
+++ C_/2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#pragma pack(1)
+struct A{
+	int a;
+	char b;
+	int c;
+};
+int main()
+{
+ struct A k={18,'j',6};
+ void *ptr=&k;
+
+ printf("%d\n",*(int*)ptr);
+ ptr = (int*)ptr+1;
+ printf("%c\n",*(char*)ptr);
+ ptr = (char*)ptr+1;
+ printf("%d\n",*(int*)ptr);
+ return 0;
+}
+
